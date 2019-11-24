b2a35c131c15030b7e6aeef81f3eae793c467352
pravir-kadian20 <56831070+pravir-kadian20@users.noreply.github.com>
2019-11-24 21:48:53 +0530

    Update searching in roated sorted array.cpp

--- searching in roated sorted array.cpp
+++ searching in roated sorted array.cpp
@@ -1,42 +1,42 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void searchinrotated(int n,int *a,int key){
-    int sp=0;
-    int ep=n-1;
-    int mid=(sp+ep)/2;
-    if(a[mid]<=key && a[ep]>=key){
-        sp=mid+1;
+int searchrotatedarray(int *a,int sp,int ep,int key){
+    if(sp>ep){
+        return -1;
+    }
+    int mid = (sp+ep)/2;
+
+    if(a[mid]==key){
+        return mid;
+    }
+    if(a[mid]>a[sp]){
+        if(a[mid]<key && key>a[sp]){
+            return searchrotatedarray(a,sp,mid-1,key);
+        }
+        else{
+            return searchrotatedarray(a,mid+1,ep,key);
+        }
     }
     else{
-        ep=mid-1;
-    }
-    while(sp<=ep){
-        mid=(sp+ep)/2;
-        if(a[mid]==key){
-            cout << "found at " << mid << " index" << endl;
-            break;
+        if(a[mid]<key && key<a[sp]){
+            return searchrotatedarray(a,mid+1,ep,key);
         }
-        if(a[mid]>key){
-            ep=mid-1;
+        else{
+            return searchrotatedarray(a,sp,mid-1,key);
         }
-        if(a[mid]<key){
-            sp=mid+1;
-        }
-    }
-    if(sp>ep){
-        cout << "not found" << endl;
     }
 }
+
 int main() {
     int n;
     cin>>n;
-    int a[100];
+    int a[1000000005];
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
     int key;
     cin>>key;
-    searchinrotated(n,a,key);
-    return 0;
+    cout << searchrotatedarray(a,0,n-1,key) << endl;
+	return 0;
 }
